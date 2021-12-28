5c758dae23d5ab994cce1cd3511f907c1d078d41
Apolgasim <81713835+ALSHAZLI@users.noreply.github.com>
2021-12-28 15:26:48 +0300

    scale it with Cluster

--- index.js
+++ index.js
@@ -4,16 +4,36 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const os = require("os");
+const cluster = require("cluster");
 
+const numCpu = os.cpus().length;
+
+console.log(numCpu);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/as.html");
 });
 
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
-    io.emit('chat message', msg);
+    io.emit("chat message", msg);
   });
 });
-server.listen(process.env.PORT || 5000, () => {
-  console.log("Backend server running on port 5000");
-});
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  server.listen(process.env.PORT || 5000, () => {
+    console.log("Backend server running on port 5000");
+    console.log(`Worker ${process.pid} started`);
+  });
+}
